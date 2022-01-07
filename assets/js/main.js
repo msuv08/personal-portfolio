6b277827337d914854937012d7a566486bbd771d
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2022-01-07 17:43:47 -0600

    Added menu dismissal on mobile upon link click

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,13 @@ const showMenu = (toggleId,navId)=>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 
