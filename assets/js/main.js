53544fb15c5e1fc4963b7a0a4dbc425ae971983d
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2022-01-07 17:57:49 -0600

    Finished header design, responsive elements added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,10 +21,36 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*===== CHANGE BACKGROUND HEADER =====*/ 
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200){
+        header.classList.add('scroll-header'); 
+    } else {
+        header.classList.remove('scroll-header');
+    }   
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*===== SHOW SCROLL TOP =====*/ 
 
