f202188be4a6a746c382a6939438b9ece3196c0a
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2022-01-07 17:38:09 -0600

    Navbar finished, working on menu updates. Added CSS & JS.

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*===== SHOW MENU =====*/
+const showMenu = (toggleId,navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+
+
+/*===== CHANGE BACKGROUND HEADER =====*/ 
+
+
+/*===== SHOW SCROLL TOP =====*/ 
+
+
+/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+
+/* Link active portfolio */ 
+
+
+/*===== SWIPER CAROUSEL =====*/ 
+
+
+/*===== GSAP ANIMATION =====*/ 
+
+
